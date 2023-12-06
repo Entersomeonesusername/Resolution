af12158602f3400c1d05eb1ba12ca4eadf64b49e
Entersomeonesusername <144603640+Entersomeonesusername@users.noreply.github.com>
2023-12-06 23:43:39 +0530

    Update gunicorn.py

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,21 @@
+# import multiprocessing
+
+# bind = f":8000"  # noqa
+
+# hard_coded_cpu_limit = min(multiprocessing.cpu_count(), 2) 
+# workers = hard_coded_cpu_limit * 2 + 1
+# threads = 4 
+# gunicorn.py
+
 import multiprocessing
 
-bind = f":8000"  # noqa
+bind = ":8000"
+workers = 3  # Adjust the number of workers based on your requirements
+threads = 4
+timeout = 120  # Increase the timeout value as needed
 
+# Optional: You can configure other Gunicorn settings here
+
+# Hard-coded CPU limit for worker count
 hard_coded_cpu_limit = min(multiprocessing.cpu_count(), 2) 
 workers = hard_coded_cpu_limit * 2 + 1
-threads = 4 
