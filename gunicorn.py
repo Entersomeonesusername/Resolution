58f7c8c56c5f92ac51ba5be670f25daeabed1cad
Entersomeonesusername <144603640+Entersomeonesusername@users.noreply.github.com>
2023-12-07 10:57:07 +0530

    Update gunicorn.py

--- gunicorn.py
+++ gunicorn.py
@@ -12,7 +12,7 @@ import multiprocessing
 bind = ":8000"
 workers = 3  # Adjust the number of workers based on your requirements
 threads = 4
-timeout = 900  # Increase the timeout value as needed
+timeout = 600  # Increase the timeout value as needed
 
 # Optional: You can configure other Gunicorn settings here
 
@@ -23,7 +23,7 @@ cpu_count = multiprocessing.cpu_count()
 desired_workers = cpu_count * 2 + 1
 
 # Ensure the number of workers is within a reasonable range
-workers = max(2, max(desired_workers, 8))
+workers =desired_workers
 
 # Log the final number of workers
 print(f"Number of Gunicorn workers: {workers}")
