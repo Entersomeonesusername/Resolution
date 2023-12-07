ad285e3d3d9b9aee414fee450bcb3313a24511c4
Entersomeonesusername <144603640+Entersomeonesusername@users.noreply.github.com>
2023-12-07 10:01:00 +0530

    Update gunicorn.py

--- gunicorn.py
+++ gunicorn.py
@@ -23,7 +23,7 @@ cpu_count = multiprocessing.cpu_count()
 desired_workers = cpu_count * 2 + 1
 
 # Ensure the number of workers is within a reasonable range
-workers = max(2, min(desired_workers, 8))
+workers = max(2, max(desired_workers, 8))
 
 # Log the final number of workers
 print(f"Number of Gunicorn workers: {workers}")
